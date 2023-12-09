13817e922393c085538d553bf2d03c730685055e
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2023-12-09 21:36:18 +0530

    Time: 151 ms (39.47%) | Memory: 18 MB (26.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,17 +1,10 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # for i in range(len(nums)):
-        #     for j in range(len(nums)-1):
-        #         if nums[j]==0:
-        #             nums[j],nums[j+1]=nums[j+1],nums[j]
-        
-        non_zero_index = 0
-
-        # Move all non-zero elements to the front
+        v = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[non_zero_index], nums[i] = nums[i], nums[non_zero_index]
-                non_zero_index += 1
+                nums[v], nums[i] = nums[i], nums[v]
+                v += 1
 
         
         
