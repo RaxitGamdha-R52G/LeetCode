40c525c58da2053ba013322a63179975a0410b48
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2023-12-09 21:07:44 +0530

    Time: 46 ms (9.99%) | Memory: 16.1 MB (82.67%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -4,21 +4,30 @@ class Solution:
         b=len(word2)
         c=abs(a-b)
         d=""
-        if c==0:
-            for i in range(0,a):
-                d+=word1[i]
-                d+=word2[i]
-        elif a-b<0:
-            for i in range(0,a):
-                d+=word1[i]
-                d+=word2[i]
+        # if c==0:
+        #     for i in range(0,a):
+        #         d+=word1[i]
+        #         d+=word2[i]
+        # elif a-b<0:
+        #     for i in range(0,a):
+        #         d+=word1[i]
+        #         d+=word2[i]
             
+        #     d+=word2[a:]
+        # else:
+        #     for i in range(0,b):
+        #         d+=word1[i]
+        #         d+=word2[i]
+            
+        #     d+=word1[b:]
+
+        for i in range(0,a if a-b<=0 else b):
+                d+=word1[i]
+                d+=word2[i]
+        if a-b<0:    
             d+=word2[a:]
-        else:
-            for i in range(0,b):
-                d+=word1[i]
-                d+=word2[i]
-            
+        elif a-b>0:
             d+=word1[b:]
+        
         return d
         
