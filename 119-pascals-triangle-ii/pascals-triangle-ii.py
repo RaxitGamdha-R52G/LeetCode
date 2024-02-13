f2aa76afbcb8fe66adbef49d937f958640e7b328
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-02-13 13:11:15 +0530

    Time: 42 ms (22.31%) | Memory: 16.5 MB (86.22%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        k=[[1]*(i+1) for i in range(rowIndex+1)]
+
+        for i in range(len(k)):
+            for j in range(len(k[i])):
+                if j ==0 or j== len(k[i])-1:
+                    continue
+                k[i][j]=k[i-1][j-1]+k[i-1][j]
+        
+        return k[len(k)-1]
