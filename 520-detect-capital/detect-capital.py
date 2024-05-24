bf07c3f92c18be8e28dd2b2bb4267cb8d6756d4b
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-05-24 22:00:22 +0530

    Time: 32 ms (76.82%) | Memory: 16.4 MB (99.14%) - LeetSync

--- 520-detect-capital/detect-capital.py
+++ 520-detect-capital/detect-capital.py
@@ -1,8 +1,9 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        w=False
-        l=[word.isupper(),word.islower(),word==word.capitalize()]
-        for i in l:
-            if i:
-                w=w or i
-        return w
+        # w=False
+        # l=[word.isupper(),word.islower(),word==word.capitalize()]
+        # for i in l:
+        #     if i:
+        #         w=w or i
+        # return w
+        return word.isupper() or word.islower() or word.istitle()
