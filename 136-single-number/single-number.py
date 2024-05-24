17459dadaafecd543ee0b9039a693fabb1378d8c
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-05-24 11:58:49 +0530

    Time: 112 ms (50.12%) | Memory: 19.1 MB (72.82%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
         l=0
         for i in nums:
             l = l ^ i
