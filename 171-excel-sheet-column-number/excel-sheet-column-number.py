bc34e76071b314a2af9a9e86bbd63af612c2ae10
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-05-25 11:01:30 +0530

    Time: 32 ms (82.58%) | Memory: 16.5 MB (87.02%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/excel-sheet-column-number.py
@@ -1,14 +1,5 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        # result=[ord(i)-64 for i in columnTitle]
-
-        # power=0
-        # final_result=0
-        # for i in result[::-1]:
-        #     final_result += i*26**power
-        #     power += 1
-        
-        # return final_result
         result=0
         power=len(columnTitle)-1
         for i in columnTitle:
