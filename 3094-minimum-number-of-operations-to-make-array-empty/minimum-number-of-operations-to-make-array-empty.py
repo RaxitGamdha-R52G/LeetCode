b6267e5edcc931e166a9a20b1f8e4a297ea767d1
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-05-29 14:52:58 +0530

    Time: 587 ms (53.37%) | Memory: 31 MB (32.59%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -1,18 +1,10 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        total_operations = 0
-        from collections import Counter
-        nums= Counter(nums)
-        for i in nums:
-            while nums[i]>0:
-                if nums[i] > 1:
-                    if nums[i] % 3 ==0  and nums[i] % 2 == 0 or (nums[i] % 3 in [0,2] and nums[i] != 2):
-                        nums[i] -= 3 
-                    else:
-                        nums[i] -= 2
-                    total_operations += 1
-                elif nums[i]==1:
-                    return -1
+        total_operations=0
+        import math
+        for value in Counter(nums).values():
+            if value == 1: return -1
+            total_operations += math.ceil(value / 3)
         return total_operations
                 
 
