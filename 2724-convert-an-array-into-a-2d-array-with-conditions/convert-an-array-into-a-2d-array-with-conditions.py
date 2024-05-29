09e6902d5d3583f461f766186b430535b47183fd
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-05-29 13:06:31 +0530

    Time: 51 ms (63.78%) | Memory: 16.4 MB (96.87%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,10 +1,11 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         result=[]
-        while nums != []:
-            a=list(set(nums))
-            result.append(a)
-            for i in a:
-                nums.remove(i)
+        a=list(set(nums))
+        result.append(a)
+        for i in a:
+            nums.remove(i)
+        if nums != []:
+            result.extend(self.findMatrix(nums))
         
         return result
