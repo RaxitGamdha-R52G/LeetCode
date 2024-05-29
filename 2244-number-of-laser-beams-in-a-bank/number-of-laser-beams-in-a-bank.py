f9007b0ed6054f83316ca7805dddde152acce7f2
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-05-29 14:04:00 +0530

    Time: 88 ms (76.76%) | Memory: 18.7 MB (80.59%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -7,17 +7,10 @@ class Solution:
             row_lst.append(i.count('1'))
         
         r1 = 0
-        r2 = 0
         for i in row_lst:
+            total_beams += r1 * i
             if i > 0:
-                if r1 == 0:
-                    r1 = i
-                    continue
-                if r2 == 0:
-                    r2 = i
-                total_beams += r1 * r2
-                r1 = r2
-                r2 = 0
+                r1 = i
         return total_beams
         
                 
