ef8f37269d58f124f1ce50de4f8407a958f414b0
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-07-24 08:23:27 +0530

    Time: 624 ms (100.00%) | Memory: 23.1 MB (75.00%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,10 +1,6 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        l_map = []
-        for num in nums:
-            d = ''.join([str(mapping[int(i)]) for i in str(num)])
-            l_map.append([int(d),num])
-        l_map.sort(key=lambda x:x[0])
-        return [i[1] for i in l_map]
+        trans_rule = str.maketrans({str(i):str(x) for i,x in enumerate(mapping)})
+        return sorted(nums, key=lambda x: int(str(x).translate(trans_rule)))
 
         
