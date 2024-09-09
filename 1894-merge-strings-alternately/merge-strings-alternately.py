82fc8a9d4066981650be3a89d28d5b8eaac7f2be
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-09-09 09:47:45 +0530

    Time: 8 ms (93.78%) | Memory: 11.6 MB (61.00%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,16 +5,17 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        result = ''
         d= min(len(word1),len(word2))
+        result = []
         i = 0
 
         while i < d:
-            result+= word1[i]+word2[i]
+            result.append(word1[i])
+            result.append(word2[i])
             i+=1
         result+= word1[i:]
         result+= word2[i:]
 
-        return result
+        return ''.join(result)
         
         
