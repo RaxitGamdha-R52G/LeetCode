d674123ef9014674e27fa2b1a38f61439d2b0b74
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-09-17 11:17:50 +0530

    Time: 61 ms (5.85%) | Memory: 16.6 MB (56.92%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,14 +1,15 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        result = ''
-        while num > 0:
-            n_z = len(str(num))-1
-            tens = 10**n_z
-            d = num
-            num %= tens
-            result += self.roman(d - num)
+        # result = ''
+        # while num > 0:
+        #     n_z = len(str(num))-1
+        #     tens = 10**n_z
+        #     d = num
+        #     num %= tens
+        #     result += self.roman(d - num)
         
-        return result
+        # return result
+        return self.roman(num)
 
     
     def roman(self,num):
@@ -21,18 +22,18 @@ class Solution:
             500:'D',
             1000:'M'
         }
-        n_z = len(str(num))-1
         answer = ''
         while num > 0:
+            n_z = len(str(num))-1
             v = 1 if str(num)[0] < '5' or str(num)[0] == '9' else 5
-            tens = v*10**n_z
+            tens = v*10**n_z if n_z > 0 else v
             if num in n.keys():
                 answer += n[num]
-                break
-            elif num+tens in n.keys():
+                num = 0
+            elif v != 5 and num+tens - num % tens in n.keys():
                 answer += n[tens]
-                answer += n[num+tens]
-                break
+                answer += n[num+tens - num % tens]
+                num = num % tens
                 
             else:
                 answer += n[tens]
