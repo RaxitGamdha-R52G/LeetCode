d8216a854f175212e5187ee35c69b2c6ccd27364
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2024-12-27 16:20:07 +0530

    Time: 65 ms (52.67%) | Memory: 18.2 MB (34.92%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,29 +1,18 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        arr = []
         if not grid:
             return 0
-        
-        def dfs(grid,i,j,arr):
-            x_l = len(grid)
-            y_l = len(grid[0])
-            arr.append((i,j))
-            per = 0
-            directions = [(-1,0),(0,-1),(1,0),(0,1)]
-
-            for dx,dy in directions:
-                t_x = i + dx
-                t_y = j + dy
-                if 0 <= t_x < x_l and 0 <= t_y < y_l and grid[t_x][t_y] == 1:
-                    if (t_x,t_y) not in arr:
-                        per += dfs(grid, t_x, t_y,arr)
-                else:
-                    per += 1
-            return per
-
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        per = 0
+        x_l = len(grid)
+        y_l = len(grid[0])
+        for i in range(x_l):
+            for j in range(y_l):
                 if grid[i][j] == 1:
-                    return dfs(grid,i,j,arr)
-        
+                    for x,y in [(-1,0),(0,-1),(1,0),(0,1)]:
+                        t_x = i + x
+                        t_y = j + y
+                        if 0 <= t_x < x_l and 0 <= t_y < y_l and grid[t_x][t_y] == 1:
+                            pass
+                        else:
+                            per += 1
+        return per
