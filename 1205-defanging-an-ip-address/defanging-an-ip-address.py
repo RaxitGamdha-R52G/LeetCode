c4ae42def8e1ce9c001db92b8ee7b5f1507274fc
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-01-09 15:20:24 +0530

    Time: 48 ms (6.94%) | Memory: 17.9 MB (8.52%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.py
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -1,3 +1,9 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        return "[.]".join(address.split("."))
+        # Solution 
+        # return "[.]".join(address.split("."))
+
+        result = ""
+        for i in address:
+            result += "[.]" if i == "." else i
+        return result
