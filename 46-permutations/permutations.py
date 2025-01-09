4a3120382493d1a4619a2a5584296e6d4af72dd5
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-01-09 12:18:17 +0530

    Time: 3 ms (35.69%) | Memory: 17.6 MB (41.00%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,7 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        from itertools import permutations
-        per_arr = list(permutations(nums))
-        result = list(map(list,per_arr))
+        # from itertools import permutations
+        # per_arr = list(permutations(nums))
+        # result = list(map(list,per_arr))
+        # return result
+        result = []
+        self.permutations(nums,[],result)
         return result
-        
+
+    def permutations(self,arr, temp, result_arr):
+        if not arr:
+            result_arr.append(temp)
+
+        for i in range(len(arr)):
+            self.permutations(arr[:i]+arr[i+1:],temp + [arr[i]],result_arr)
