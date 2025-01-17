4b1cd7429b49c96fbfed9de2880446bd4f8a7f35
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-01-18 01:18:32 +0530

    Time: 49 ms (44.30%) | Memory: 28.8 MB (67.24%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,14 +1,25 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        left_min = 1
-        hmap = defaultdict(int)
-        for i in nums:
-            if i > 0:
-                hmap[i] = 1
-                while hmap[left_min] > 0:
-                    del hmap[left_min]
-                    left_min += 1
-                
+        # Solution 1
+        # left_min = 1
+        # hmap = defaultdict(int)
+        # for i in nums:
+        #     if i > 0:
+        #         hmap[i] = 1
+        #         while hmap[left_min] > 0:
+        #             del hmap[left_min]
+        #             left_min += 1
+        # return left_min
+
+        # Solution 2
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] -1], nums[i] = nums[i], nums[nums[i] -1]
         
-        return left_min
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return n + 1
         
