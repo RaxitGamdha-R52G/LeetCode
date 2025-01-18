5c6b9b47b0f8b2f54de809d2dc9ea4ae6e69b762
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-01-18 19:18:39 +0530

    Time: 7 ms (94.13%) | Memory: 18.1 MB (31.04%) - LeetSync

---
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        # f_word = []
+        # v = set(letters)
+        # for word in words:
+        #     flag = 1
+        #     for char in word:
+        #         if char not in v:
+        #             flag = 0
+        #             break
+        #     if flag:
+        #         f_word.append(word)
+        result = []
+        length = len(words)
+
+        def backtrack(a_score, letters, curr):
+            if curr == length:
+                result.append(a_score)
+                return
+            
+            flag = True
+            temp = 0
+            v = letters.copy()
+            for ch in words[curr]:
+                if v[ch] > 0:
+                    temp += score[ord(ch) - 97]
+                    v[ch] -= 1
+                else:
+                    flag = False
+                    break
+            
+            if flag:
+                backtrack(a_score+temp,v,curr+1)          
+            backtrack(a_score,letters,curr+1)
+        
+        backtrack(0, Counter(letters), 0)
+        return max(result)
