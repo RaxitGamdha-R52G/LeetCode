f02b1a52919843b62624cac39c69c7cf064361f4
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-01-20 14:45:36 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (10.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,25 +5,38 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        list3=None
+        # list3=None
 
-        while list1!=None or list2!=None:
-            q=list1 if list1 is not None else "__"
-            p=list2 if list2 is not None else "__"
-            if (q=="__" ) or (p!="__" and p.val<=q.val):
-                Node=p
-                list2=list2.next
-            else:
-                Node=q
-                list1=list1.next
-            if list3==None:
-                list3=Node
-                list3_e=Node
-            else:
-                list3_e.next=Node
-                list3_e=list3_e.next
+        # while list1!=None or list2!=None:
+        #     q=list1 if list1 is not None else "__"
+        #     p=list2 if list2 is not None else "__"
+        #     if (q=="__" ) or (p!="__" and p.val<=q.val):
+        #         Node=p
+        #         list2=list2.next
+        #     else:
+        #         Node=q
+        #         list1=list1.next
+        #     if list3==None:
+        #         list3=Node
+        #         list3_e=Node
+        #     else:
+        #         list3_e.next=Node
+        #         list3_e=list3_e.next
         
-        return list3
+        # return list3
 
+        def sor(list1,list2):
+            if not list1:
+                return list2
+            if not list2:
+                return list1
+            if list1.val <list2.val:
+                list1.next = sor(list1.next,list2)
+                return list1
+            else:
+                list2.next = sor(list1,list2.next)
+                return list2
+            
+        return sor(list1,list2)
                 
         
