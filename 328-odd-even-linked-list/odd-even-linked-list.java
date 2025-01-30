bfa95b7b0056f1a12847e064b86ca231d3ba83c8
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-01-30 23:43:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (63.04%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -13,29 +13,47 @@ class Solution {
         if(head == null){
             return null;
         }
-        ListNode odd = new ListNode(1); // dummy odd node
-        ListNode even = new ListNode(0); // dummy even node
+        // Solution 1
+        // ListNode odd = new ListNode(1); // dummy odd node
+        // ListNode even = new ListNode(0); // dummy even node
 
-        ListNode p = head;
-        ListNode t_o = odd;
-        ListNode t_e = even;
-        int count = 1;
-        while (p!=null){
-            boolean isOdd = ((count)%2==1);
-            ListNode n = new ListNode(p.val);
-            if (isOdd){
-                t_o.next = n;
-                t_o = t_o.next;
+        // ListNode p = head;
+        // ListNode t_o = odd;
+        // ListNode t_e = even;
+        // int count = 1;
+        // while (p!=null){
+        //     boolean isOdd = ((count)%2==1);
+        //     ListNode n = new ListNode(p.val);
+        //     if (isOdd){
+        //         t_o.next = n;
+        //         t_o = t_o.next;
 
-            }else{
-                t_e.next = n;
-                t_e = t_e.next;
-            }
-            p = p.next;
-            count++;
+        //     }else{
+        //         t_e.next = n;
+        //         t_e = t_e.next;
+        //     }
+        //     p = p.next;
+        //     count++;
+        // }
+        // t_o.next = even.next;
+        // return odd.next;
+
+        // Solution 2
+
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        ListNode odd = p1;
+        ListNode even = p2;
+
+        while( p1 != null && p2 != null && p2.next!= null){
+            p1.next = p2.next;
+            p2.next = p2.next.next;
+            p1 = p1.next;
+            p2 = p2.next;
         }
-        t_o.next = even.next;
-        return odd.next;
-        
+
+        p1.next = even;
+
+        return odd;
     }
 }
