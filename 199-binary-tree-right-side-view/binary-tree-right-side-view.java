34a157113b9e7235f8400d2b3f96ce6ceba08433
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-01 00:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (78.99%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,34 +14,52 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
+    int max_depth = -1;
+
+    public void rightSide(TreeNode root, int level, List<Integer> res){
         if(root == null){
-            return new ArrayList<Integer>();
+            return;
         }
 
+        if(max_depth < level){
+            res.add(root.val);
+            max_depth = level;
+        }
+
+        rightSide(root.right, level+1, res);
+        rightSide(root.left, level+1, res);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        // if(root == null){
+        //     return new ArrayList<Integer>();
+        // }
+
+        // List<Integer> res = new ArrayList<Integer>();
+        // Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        // queue.add(root);
+        // int size = queue.size();
+        // while (size > 0){
+        //     TreeNode rightMost = null;
+        //     for(int i = 0; i< size; i++){
+        //         TreeNode n = queue.remove();
+        //         rightMost = n;
+
+        //         if(n.left != null){
+        //             queue.add(n.left);
+        //         }
+        //         if(n.right != null){
+        //             queue.add(n.right);
+        //         }
+        //     }
+        //     res.add(rightMost.val);
+        //     size = queue.size();
+        // }
+
+        // return res;
         List<Integer> res = new ArrayList<Integer>();
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-
-        queue.add(root);
-        int size = queue.size();
-        while (size > 0){
-            TreeNode rightMost = null;
-            for(int i = 0; i< size; i++){
-                TreeNode n = queue.remove();
-                rightMost = n;
-
-                if(n.left != null){
-                    queue.add(n.left);
-                }
-                if(n.right != null){
-                    queue.add(n.right);
-                }
-            }
-            res.add(rightMost.val);
-            size = queue.size();
-        }
-
+        rightSide(root, 0, res);
         return res;
-        
     }
 }
