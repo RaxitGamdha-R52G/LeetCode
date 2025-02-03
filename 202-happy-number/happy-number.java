359a02082d8df5b9c14db5190b7895b38168ca4b
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-03 13:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (21.48%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,19 +1,34 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> hs = new HashSet<Integer>();
-        while(n > 1){
-            int sum = 0;
-            while(n > 0){
-                int d = n % 10;
-                n /= 10;
-                sum += Math.pow(d,2);
-            }
-            if (hs.contains(sum)){
-                return false;
-            }
-            hs.add(sum);
-            n = sum;
+        // Solution 1
+        // HashSet<Integer> hs = new HashSet<Integer>();
+        // while(n > 1){
+        //     int sum = 0;
+        //     while(n > 0){
+        //         int d = n % 10;
+        //         n /= 10;
+        //         sum += Math.pow(d,2);
+        //     }
+        //     if (hs.contains(sum)){
+        //         return false;
+        //     }
+        //     hs.add(sum);
+        //     n = sum;
+        // }
+        // return true;
+
+        // Solution 2
+        if(n == 1 || n == 7){
+            return true;
+        }else if(n < 10){
+            return false;
         }
-        return true;
+        int sum = 0;
+        while (n > 0){
+            int d = n %10;
+            n /= 10;
+            sum += Math.pow(d,2);
+        }
+        return isHappy(sum);
     }
 }
