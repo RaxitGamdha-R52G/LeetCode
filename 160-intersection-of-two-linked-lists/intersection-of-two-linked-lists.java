c671b0b0c54017f02a69733d4cb9db887f9e1e80
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-06 10:41:57 +0530

    Time: 1 ms (99.92%) | Memory: 48.7 MB (44.93%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,22 +10,35 @@
  * }
  */
 public class Solution {
+    public int len(ListNode head){
+        if(head == null) return 0;
+        return 1 + len(head.next);
+    }
+
+    public ListNode moveHead(ListNode head, int shift){
+        for(int i = 0; i< shift; i++){
+            head = head.next;
+        }
+        return head;
+    }
+
+    public ListNode compare(ListNode headA, ListNode headB){
+        if(headA == null || headB == null) return null;
+        if(headA == headB) return headA;
+        return compare(headA.next, headB.next);
+    }
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> s = new HashSet<>();
+        int len1 = len(headA);
+        int len2 = len(headB);
 
-        while(headA != null){
-            s.add(headA);
-            headA = headA.next;
-    
+        if(len1 > len2){
+            headA = moveHead(headA, len1-len2);
+        }else if(len1 < len2){
+            headB = moveHead(headB, len2-len1);
         }
 
-        while(headB != null){
-            if (s.contains(headB)){
-                return headB;
-            }
-            headB = headB.next;
-        }
-
-        return null;
+        ListNode result = compare(headA,headB);
+        return result;
     }
 }
