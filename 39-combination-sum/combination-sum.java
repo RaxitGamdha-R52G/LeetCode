366ed226f8fbde80e6b4a03b14c700d98f32b0e8
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-08 02:01:17 +0530

    Time: 1 ms (99.87%) | Memory: 44.5 MB (80.34%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,53 @@
 class Solution {
-    private List<List<Integer>> result = new ArrayList<List<Integer>>();
-    private void backtrack(int[] candidates, int target, List<Integer> path, int start ){
-        if(target == 0){
-            // Collections.sort(path);
-            // if(!result.contains(path))result.add(path);
+    // private List<List<Integer>> result = new ArrayList<List<Integer>>();
+    // private void backtrack(int[] candidates, int target, List<Integer> path, int start ){
+    //     if(target == 0){
+    //         // Collections.sort(path);
+    //         // if(!result.contains(path))result.add(path);
+    //         result.add(new ArrayList<>(path));
+    //         return;
+    //     }
+
+    //     // for (int i = start; i < candidates.length; i++){
+    //     //     int diff = target - candidates[i];
+    //     //     // if(diff >= 0){
+    //     //     //     List<Integer> temp = new ArrayList<Integer>(path);
+    //     //     //     temp.add(num);
+    //     //     //     backtrack(candidates, diff, temp);
+    //     //     // }
+    //     //     if(diff >= 0){
+    //     //         path.add(candidates[i]);
+    //     //         backtrack(candidates, diff, path, i);
+    //     //         path.remove(path.size()-1);
+    //     //     }
+    //     }
+    // }
+
+    private List<List<Integer>> result = new ArrayList<>();
+    private List<Integer> path = new ArrayList<>();
+    private void backtrack(int[] candidates, int target, int i ){
+        if( target == 0){
             result.add(new ArrayList<>(path));
             return;
         }
 
-        for (int i = start; i < candidates.length; i++){
-            int diff = target - candidates[i];
-            // if(diff >= 0){
-            //     List<Integer> temp = new ArrayList<Integer>(path);
-            //     temp.add(num);
-            //     backtrack(candidates, diff, temp);
-            // }
-            if(diff >= 0){
-                path.add(candidates[i]);
-                backtrack(candidates, diff, path, i);
-                path.remove(path.size()-1);
-            }
+        if (i >= candidates.length) return;
+
+
+        int diff = target - candidates[i];
+        if(diff >= 0){
+            path.add(candidates[i]);
+            backtrack(candidates, diff, i);
+            path.remove(path.size()-1);
         }
-    }
+        backtrack(candidates, target, i+1);
+
+        }
+    
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backtrack(candidates,target, new ArrayList<>(), 0);
+        // backtrack(candidates,target, new ArrayList<>(), 0);
+        backtrack(candidates,target, 0);
         return result;
     }
 }
