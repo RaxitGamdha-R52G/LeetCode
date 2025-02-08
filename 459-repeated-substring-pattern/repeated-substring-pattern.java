1afb544f9502fcd78c69bcbbb74fd14e0700d901
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-08 22:02:36 +0530

    Time: 6 ms (97.66%) | Memory: 45.3 MB (58.90%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,6 +1,20 @@
 class Solution {
+    private boolean check(String s, String str){
+        return str.repeat(s.length()/str.length()).equals(s);
+    }
+
     public boolean repeatedSubstringPattern(String s) {
-        String sub = s+s;
-        return sub.substring(1,sub.length()-1).contains(s);
+        // String sub = s+s;
+        // return sub.substring(1,sub.length()-1).contains(s);
+        int slen = s.length();
+        for(int i = 1; i*i <= slen; i++){
+            if(slen %i == 0){
+                if(i != slen && check(s, s.substring(0,i))) return true;
+                int len2 = slen/i;
+                if(len2 != slen && check(s, s.substring(0,len2))) return true;
+            }
+        }
+
+        return false;
     }
 }
