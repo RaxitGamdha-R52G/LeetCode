29c34cb0ef0d4b7806a253f726b14e4df3ec2990
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-12 00:59:52 +0530

    Time: 1 ms (99.46%) | Memory: 42 MB (88.37%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,9 +1,31 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        if(!s.contains(part)){
-            return s;
+        // if(!s.contains(part)){
+        //     return s;
+        // }
+        // s = s.replaceFirst(part,"");
+        // return removeOccurrences(s, part);
+        char[] targetArr = part.toCharArray();
+        char[] stack = new char[s.length()];
+        int stackSize = 0;
+        char t_Char = targetArr[targetArr.length-1];
+
+        for(char c_Char: s.toCharArray()){
+            stack[stackSize++] = c_Char;
+
+            if(t_Char == c_Char && stackSize >= targetArr.length){
+                int i = stackSize-1, j = targetArr.length -1;
+                while(j>=0 && stack[i] == targetArr[j]){
+                    i--;
+                    j--;
+                }
+
+                if(j < 0){
+                    stackSize = i+1;
+                }
+            }
         }
-        s = s.replaceFirst(part,"");
-        return removeOccurrences(s, part);
+
+        return new String(stack, 0, stackSize);
     }
 }
