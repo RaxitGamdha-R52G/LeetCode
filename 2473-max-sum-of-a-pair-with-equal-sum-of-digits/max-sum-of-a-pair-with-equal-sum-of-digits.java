8e50d2f0ba6bb08885f51bdae5eb2ee5ef6fc237
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-13 00:29:41 +0530

    Time: 18 ms (100.00%) | Memory: 56.3 MB (72.23%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        Map<Integer,Integer> hs = new HashMap<>();
+        // Map<Integer,Integer> hs = new HashMap<>();
+        int[] arr = new int[100];
         int ans = -1;
         for(int num: nums){
             int sum = 0;
@@ -9,10 +10,14 @@ class Solution {
                 sum += temp%10;
                 temp /= 10;
             }
-            if(hs.containsKey(sum)){
-                ans = Math.max(ans, hs.get(sum)+num);
+            // if(hs.containsKey(sum)){
+            //     ans = Math.max(ans, hs.get(sum)+num);
+            // }
+            // hs.merge(sum, num, Math::max);
+            if(arr[sum]!= 0){
+                ans = Math.max(ans,arr[sum]+num);
             }
-            hs.merge(sum, num, Math::max);
+            arr[sum] = Math.max(arr[sum],num);
         }
 
         return ans;
